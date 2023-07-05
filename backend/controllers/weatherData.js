d36eb26f8488b36f11853bb7611185dc653bb5d8
gaugust2 <gaugust2@uwo.ca>
2023-07-05 19:28:55 -0400

    Format the weather data sent

--- backend/controllers/weatherData.js
+++ backend/controllers/weatherData.js
@@ -5,7 +5,26 @@ weatherRouter.get('/:city', async (request, response) => {
     const city = request.params.city
     const weather = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${config.OPENWEATHER_API_KEY}`)
     const weatherData = await weather.json()
-    response.json(weatherData)
+
+
+    const filteredData = filteredWeatherData(weatherData)
+    response.json(filteredData)
 })
+
+const filteredWeatherData = (data) => {
+    const object = {
+        city: data.name,
+        temperature: convertToCelsius(data.main.temp),
+        feelsLike: convertToCelsius(data.main.feels_like),
+        humidity: data.main.humidity,
+        windSpeed: data.wind.speed,
+        icon: data.weather[0].icon
+    }
+    return object
+}
+
+function convertToCelsius(temperature){
+    return Math.round((temperature - 273.15) * 100)/100
+}
 
 module.exports = weatherRouter
