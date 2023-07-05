65c24787f0acee767c5e325ffe399132b56c76d1
gaugust2 <gaugust2@uwo.ca>
2023-07-05 18:53:20 -0400

    Middleware

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,19 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 const weatherRouter = require('./controllers/weatherData')
 const config = require('./utils/config')
+const {info, error} = require('./utils/logger')
+const middleware = require('./utils/middleware')
 
 app.use(express.json())
+app.use(cors())
+app.use(middleware.requestLogger)
+//Important that requestlogger is used before using the weatherRouter
 app.use('/api/weather', weatherRouter)
 
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 app.get('/', (request, response) => {
     response.send('<h1>welcome to the first request</h1>')
@@ -15,5 +23,5 @@ app.get('/', (request, response) => {
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+    info(`Server running on port ${PORT}`)
 })
