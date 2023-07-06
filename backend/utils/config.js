4f9dd19e9075a89efebcc782ddc3901d7d56d6d1
gaugust2 <gaugust2@uwo.ca>
2023-07-06 01:07:48 -0400

    Update config for mongodb

--- backend/utils/config.js
+++ backend/utils/config.js
@@ -1,9 +1,13 @@
 require('dotenv').config()
+
 const PORT = process.env.PORT
 
 const OPENWEATHER_API_KEY = process.env.WEATHER_API_KEY
 
+const MONGODB_LINK = process.env.MONGODB_LINK
+
 module.exports = {
     PORT,
-    OPENWEATHER_API_KEY
+    OPENWEATHER_API_KEY,
+    MONGODB_LINK
 }
