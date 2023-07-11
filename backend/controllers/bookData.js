e54a3e0d75a4d65c452b4abc71886eca7e466750
gaugust2 <gaugust2@uwo.ca>
2023-07-10 23:52:20 -0400

    Can show data for a single volume

--- backend/controllers/bookData.js
+++ backend/controllers/bookData.js
@@ -1,16 +1,7 @@
 const bookRouter = require('express').Router()
 const config = require('../utils/config')
 
-bookRouter.post('/all', async (request, response) => {
-    const title = request.body.title
-    const author = request.body.author
-
-    const books = await fetch(`https://www.googleapis.com/books/v1/volumes?q=${title}+inauthor:${author}&printType=books&orderBy=relevance&key=${config.BOOKS_API_KEY}`)
-    const bookData = await books.json()
-
-    response.json(bookData)
-})
-
+//Give list of names and ids from search parameters
 bookRouter.post('/names', async (request, response) => {
     const title = request.body.title
     const author = request.body.author
@@ -19,8 +10,6 @@ bookRouter.post('/names', async (request, response) => {
     const bookData = await books.json()
 
     const parsed = Object.values(bookData.items)
-    //const names = parsed.map(object => object.volumeInfo.title)
-    //const ids = parsed.map(object => object.id)
 
     const objects = parsed.map(object => {
         return {
@@ -28,14 +17,18 @@ bookRouter.post('/names', async (request, response) => {
             name: object.volumeInfo.title
         }
     })
+    
     response.json(objects)
 })
 
+//Give singular data
 bookRouter.get('/:id', async (request, response) => {
     const id = request.params.id
 
-    const books = await fetch(`https://www.googleapis.com/books/v1/volumes/JHEkAQAAMAAJ?key=${config.BOOKS_API_KEY}`)
+    const books = await fetch(`https://www.googleapis.com/books/v1/volumes/${id}?key=${config.BOOKS_API_KEY}`)
+    const bookData = await books.json()
 
+    response.json(bookData)
 })
 
 
