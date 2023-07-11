bf8bd89c5a2597252f8ede13d08a5c7d60c04a1d
gaugust2 <gaugust2@uwo.ca>
2023-07-11 15:02:50 -0400

    Created model for gta data

--- backend/controllers/gtaData.js
+++ backend/controllers/gtaData.js
@@ -1,31 +1,22 @@
 const gtaRouter = require('express').Router()
 const config = require('../utils/config')
 const mongoose = require('mongoose')
-
-mongoose.set('strictQuery', true);
-mongoose.connect(config.MONGODB_LINK);
-const database = mongoose.connection
-database.on('error', (error) => { console.log(error) })
-database.once('connected', () => {
-    console.log('Database Connected');
-})
+const Weapon = require('../models/gtaWeapon')
 
 gtaRouter.get('/weapons/:name', async (request,response) => {
     const name = request.params.name
 
-    database.collection('gta-weapons').findOne({name:name}, function (error, data) {
-        if (error) {
-            response.send(error)
-        } else {
-            response.json(data)
-        }
-    })
+    const object = await Weapon.findOne({name:name})
+    response.json(object)
 })
 
 gtaRouter.get('/weapons', async(request, response) => {
-    const objects = await database.collection('gta-weapons').find({}).toArray()
+    const objects = await Weapon.find({})
+
     const weaponNames = objects.map(object => object.name)
     response.json(weaponNames)
+
+
 })
 
 gtaRouter.get('/vehicles/:name', async (request, response) => {
--- backend/models/gtaWeapon.js
+++ backend/models/gtaWeapon.js
@@ -1,11 +1,12 @@
 const mongoose = require('mongoose')
 
 const Schema = mongoose.Schema
-const championSchema = new Schema({
+const weaponSchema = new Schema({
     name:{
         type:String
     }
 })
 
 
-module.exports = mongoose.model('Champion', championSchema, 'champions')
+
+module.exports = mongoose.model('Weapon', weaponSchema, 'gta-weapons')
