af951c01d46133d02a45d13f5a3cfcdde64b554c
gaugust2 <gaugust2@uwo.ca>
2023-07-12 12:39:15 -0400

    Initial homepage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,36 @@
-function App() {
+import {
+  BrowserRouter as Router,
+  Routes, Route, Link
+} from 'react-router-dom'
+import Home from './pages/Home'
+
+
+const App = () => {
+
+  const padding = {
+    padding: 5
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <div>
+        <Link style={padding} to="/">Home</Link>
+        <Link style={padding} to="/notes">notes</Link>
+        <Link style={padding} to="/users">users</Link>
+      </div>
+
+      <Routes>
+        <Route path="/notes" element={<h2>hello notes</h2>} />
+        <Route path="/users" element={<h2>hello users</h2>} />
+        <Route path="/" element={<Home/>} />
+      </Routes>
+
+      <div>
+        <a href="https://github.com/gaugust2">Github</a>
+        <a href="https://www.linkedin.com/in/gerald-augustinepillai-8b3832260">Linkedin</a>
+      </div>
+    </Router>
+  )
 }
 
-export default App;
+export default App
