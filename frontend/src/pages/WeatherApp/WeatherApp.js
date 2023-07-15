713737f8688c8d722dcb22a0a824c525f97099cd
gaugust2 <gaugust2@uwo.ca>
2023-07-14 20:36:24 -0400

    Creating weather app

---
+++ frontend/src/pages/WeatherApp/WeatherApp.js
@@ -0,0 +1,15 @@
+import './WeatherApp.css'
+
+const WeatherApp = () => {
+    return (
+        <div class="root-div-weather">
+            <form>
+                <h1>Welcome to my Weather App</h1>
+                <label>Enter a city name:</label>
+                <input type="text"></input>
+            </form>
+        </div>
+    )
+}
+
+export default WeatherApp
