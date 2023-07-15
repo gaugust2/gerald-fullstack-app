44e21ea78395cfdba1757e8e6c0a4488f49e0aec
gaugust2 <gaugust2@uwo.ca>
2023-07-14 21:36:42 -0400

    Successful get request to backend

--- frontend/src/pages/WeatherApp/WeatherApp.js
+++ frontend/src/pages/WeatherApp/WeatherApp.js
@@ -1,12 +1,47 @@
 import './WeatherApp.css'
+import { useState } from 'react'
+import weatherService from '../../services/weatherService'
+import axios from 'axios'
+
+const Weather = ({weather}) => <div>{weather.city}{weather.temperature}</div>
+
+const WeatherData = ({dataToShow}) => {
+    return(
+        <div>
+            {dataToShow.temperature}
+        </div>
+    )
+}
 
 const WeatherApp = () => {
+    const [cityName, setCityName] = useState('')
+    const [cityData, setCityData] = useState('')
+
+    const handleNameChange = (event) => {
+        setCityName(event.target.value)
+    }
+
+    const showData = (event) => {
+        event.preventDefault()
+        try {
+            weatherService.getData(cityName)
+                .then(response => {
+                    console.log(response.data)
+                    setCityData(response.data)
+                })
+        } catch (err) {
+            console.log(err)
+        }
+    }
+
     return (
-        <div class="root-div-weather">
-            <form>
+        <div className="root-div-weather">
+            <form onSubmit={showData}>
                 <h1>Welcome to my Weather App</h1>
                 <label>Enter a city name:</label>
-                <input type="text"></input>
+                <input type="text" onChange={handleNameChange}></input>
+                <button type="submit">Submit</button>
+                <WeatherData dataToShow={cityData}/>
             </form>
         </div>
     )
---
+++ frontend/src/services/weatherService.js
@@ -0,0 +1,8 @@
+import axios from 'axios'
+const baseUrl = 'http://localhost:3001/api/weather/'
+
+const getData = (name) => {
+    return axios.get(`${baseUrl}/${name}`)
+  }
+
+export default {getData}
