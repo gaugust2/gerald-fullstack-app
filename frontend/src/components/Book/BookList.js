83370d3ac6cae9434a664be346b6c4ac975c79ad
gaugust2 <gaugust2@uwo.ca>
2023-07-17 16:27:11 -0400

    Header and notice for table

--- frontend/src/components/Book/BookList.js
+++ frontend/src/components/Book/BookList.js
@@ -2,21 +2,28 @@ import BookRow from "./BookRow"
 
 const BookList = ({ data, handleShow }) => {
     return (
-        <table className="table table-hover">
-            <thead className="thead-dark">
-                <tr>
-                    <th scope="col">Book ID</th>
-                    <th scope="col">Title</th>
-                    <th scope="col">Subtitle</th>
-                    <th scope="col">Author</th>
-                </tr>
-            </thead>
-            <tbody>
-                {data.map(object => {
-                    return <BookRow entry={object} handleShow={handleShow} />
-                })}
-            </tbody>
-        </table>
+        <div className="table-container">
+            <br/>
+            <h5>Click on a row to view additional contents</h5>
+            <table className="table table-hover">
+                <thead className="thead-dark">
+                    <tr>
+                        <th scope="col">Book ID</th>
+                        <th scope="col">Title</th>
+                        <th scope="col">Subtitle</th>
+                        <th scope="col">Author</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {data.map(object => {
+                        return <BookRow entry={object} handleShow={handleShow} />
+                    })}
+                </tbody>
+            </table>
+            <h6>This data is retrieved from the Google Books Api.</h6><p> Note that some of the data may be incorrect
+                 or in an irregular format, and that only 10 relevant results are returned for each search.</p>
+        </div>
+
     )
 }
 
