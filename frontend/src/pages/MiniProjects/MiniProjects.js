4c41c8295259d7879e2019405a3a429cef1242fe
gaugust2 <gaugust2@uwo.ca>
2023-07-17 20:25:10 -0400

    Stacked cards vertically

--- frontend/src/pages/MiniProjects/MiniProjects.js
+++ frontend/src/pages/MiniProjects/MiniProjects.js
@@ -3,35 +3,58 @@ import './MiniProjects.css'
 const MiniProjects = () => {
     return (
         <div className="root-div-projects">
-            <h3>This is a list of all the little backend projects I have worked on so far:</h3>
-
-            <div class="row justify-content-around">
-                <div class="col-sm-5">
-                    <div class="card">
-                        <a href="/weatherapp">
-                            <div class="card-body">
-                                <h4 class="card-title">Weather app</h4>
-                                <p class="card-text">Enter the name of a city and see up-to-date weather data. The icon is a visual for the weather condition.</p>
-                            </div>
-                            <img class="card-img-bottom" src="https://sample-fullstack-bucket.s3.us-east-2.amazonaws.com/WeatherApp-Screenshot.png" alt="Card cap"></img>
-                        </a>
+          <h3>This is a list of all the little backend projects I have worked on so far:</h3>
+      
+          <div className="projects-table-container">
+            <table>
+              <tbody>
+                <tr className="projects-row">
+                  <td>
+                    <div className="card mb-5">
+                      <a href="/weatherapp">
+                        <div className="card-body">
+                          <h4 className="card-title">Weather app</h4>
+                          <p className="card-text">Enter the name of a city and see up-to-date weather data.
+                            The icon is a visual for the weather condition.</p>
+                        </div>
+                        <img className="card-img-bottom" src="https://sample-fullstack-bucket.s3.us-east-2.amazonaws.com/WeatherApp-Screenshot.png" alt="Card cap" />
+                      </a>
                     </div>
-                </div>
-                <div class="col-sm-5">
-                    <div class="card">
-                        <a href="/bookapp">
-                            <div class="card-body">
-
-                                <h5 class="card-title">Book app</h5>
-                                <h4>Book body</h4>
-                            </div>
-                        </a>
+                  </td>
+                </tr>
+                <tr className="projects-row">
+                  <td>
+                    <div className="card mb-5">
+                      <a href="/bookapp">
+                        <div className="card-body">
+                          <h4 className="card-title">Book app</h4>
+                          <p className="card-text">Enter a book title, author, or both, and receive up to 10 relevant results.
+                            Click on one of the results to see further details.</p>
+                        </div>
+                        <img className="card-img-bottom" src="https://sample-fullstack-bucket.s3.us-east-2.amazonaws.com/BookApp-Screenshot.png" alt="Card cap" />
+                      </a>
                     </div>
-                </div>
-            </div>
-
+                  </td>
+                </tr>
+                <tr className="projects-row">
+                  <td>
+                    <div className="card mb-5">
+                      <a href="/carapp">
+                        <div className="card-body">
+                          <h4 className="card-title">Car app</h4>
+                          <p className="card-text">Coming soon</p>
+                        </div>
+                      </a>
+                    </div>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
         </div>
-    )
+      );
+      
+
 }
 
 export default MiniProjects
