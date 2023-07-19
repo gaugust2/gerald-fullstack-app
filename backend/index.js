b5f3786d936ce8ce53571d87993347cdc4204f97
gaugust2 <gaugust2@uwo.ca>
2023-07-19 12:49:09 -0400

    Undo port change

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ const app = require('./app')
 const config = require('./utils/config')
 const {info, error} = require('./utils/logger')
 
-const PORT = process.env.PORT || 80
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     info(`Server running on port ${PORT}`)
 })
