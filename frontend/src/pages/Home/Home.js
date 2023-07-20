a75687777bea007d0f12ba9c79975a533305a896
gaugust2 <gaugust2@uwo.ca>
2023-07-20 10:49:24 -0400

    Change source code url

--- frontend/src/pages/Home/Home.js
+++ frontend/src/pages/Home/Home.js
@@ -9,7 +9,7 @@ const Home = () => {
             <h2>Welcome to the home page<Dots/></h2>
             <p>This project will have various types of interactive pages, fetching data from the backend and displaying it to the user.<br />
                 The frontend is made with React and Bootstrap. The backend uses Express and MongoDB.<br />
-                Check out the source code for this project <a href="https://github.com/gaugust2/sample-fullstack-app" target="_blank" rel="noopener noreferrer">here</a>.</p>
+                Check out the source code for this project <a href="https://github.com/gaugust2/gerald-fullstack-app" target="_blank" rel="noopener noreferrer">here</a>.</p>
         </div>
     )
 }
